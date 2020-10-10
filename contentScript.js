00ee8ead9e2bb5d832dd30210a9889c45af6c53b
Nam Anh Nguyen <namanh11611@gmail.com>
2020-10-10 20:41:49 +0700

    Add feature remove ads for new version

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,50 @@
 // Remove banner
-var warningBanner = document.querySelector(".fbPageBanner");
-if (warningBanner) {
-  warningBanner.style.display = "none";
+$(".fbPageBanner").css("display", "none");
+
+
+// Remove ads
+var adsDiv = getAdsByLabel();
+var adsLength = 0;
+if (adsDiv.length > 0) {
+  adsLength = adsDiv.length;
+  adsDiv.forEach(element => {
+    findClosestDiv(element);
+  });
+}
+
+$(document).scroll(function() {
+  adsDiv = getAdsByLabel();
+  if (adsDiv.length > adsLength) {
+    for (let i = adsLength; i < adsDiv.length; i++) {
+      findClosestDiv(adsDiv[i]);
+    }
+    adsLength = adsDiv.length;
+  }
+});
+
+function getAdsByLabel() {
+  // English
+  let adsDivByLanguage = $("div[aria-label='Sponsored']");
+  
+  // Vietnamese
+  if (!adsDivByLanguage.length) {
+    adsDivByLanguage = $("div[aria-label='Được tài trợ']");
+  }
+  
+  return adsDivByLanguage;
+}
+
+function findClosestDiv(label) {
+  if (label) {
+    console.log("label", label);
+    hideAdsDiv(label.closest("[data-pagelet='FeedUnit_1']"));
+    hideAdsDiv(label.closest("[data-pagelet='FeedUnit_{n}']"));
+  }
+}
+
+function hideAdsDiv(ads) {
+  if (ads) {
+    console.log("ads", ads);
+    ads.style.display = "none";
+  }
 }
